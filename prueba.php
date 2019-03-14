3bd2fa3d739d86f737ef459116d889752464b513
DiegoBP12 <1717110611@utectulancingo.edu.mx>
2019-03-14 11:48:52 -0600

    Cambios en el formulario contacto.html, prueba.php y master.css

--- prueba.php
+++ prueba.php
@@ -1,36 +1,48 @@
+<!DOCTYPE html>
 <html>
-    <body>
+<head>
+    <title>Contacto</title>
+    <meta charset="utf-8">
+</head>
+<body>
     <?php
-        $email = $_POST['correo'];
-        $nombre = $_POST['nombre'];
-        $asunto = $_POST['asunto'];
-        $mensaje = $_POST['descripcion'];
-        
         $cuerpo = '
 <html>
 <head>
- <title>Mail HTML</title>
+ <title></title>
 </head>
 <body>
-'.$POST['cuerpo'].'
+'.$_POST['descripcion'].'
 </body>
 </html>
 ';
 
-        
+//para el envío en formato HTML
 $headers  = "MIME-Version: 1.0\r\n";
 $headers .= "Content-type: text/html; charset=iso-8859-1\r\n";
-//dirección del remitente
-$headers .= "From: ".$email."\r\nContent-type: text/html\r\n";
 
+//dirección del remitente
+$headers .= "From: ".$_POST['nombre']." <".$_POST['email'].">\r\n";
+
+//Una Dirección de respuesta, si queremos que sea distinta que la del remitente
+//$headers .= "Reply-To: ejemplo@hotmail.com\r\n";
+
+//Direcciones que recibián copia
+//$headers .= "Cc: ejemplo2@gmail.com\r\n";
+
+//direcciones que recibirán copia oculta
+//$headers .= "Bcc: ejemplo3@yahoo.com\r\n";
 if(isset($_POST['enviar'])){
-	if(mail($_POST['receptor'],$_POST['asunto'],$cuerpo,$headers)){
-		echo "<script>alert('Email enviado correctamente');</script>";
-        
+	if(mail('diegobolpar@hotmail.com',$_POST['asunto'],$cuerpo,$headers)){
+        header("Location:exitoso.html");
 	}else{
-		echo "<script>alert('No se pudo enviar el mail.');</script>";
+        echo "<script>alert('No se pudo enviar el mail.');</script>";
+        
 	}
 }
-?>
-    </body>
+    ?>;
+    
+
+    
+</body>
 </html>
